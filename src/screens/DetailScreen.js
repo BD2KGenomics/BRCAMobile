0e90968248b317c6fda5fe02a8ee040ea259008a
Faisal <falquaddoomi@ucla.edu>
2017-07-18 12:06:00 +0200

    Details screen can now receive a variant_id as a prop and load its data on its own if no data is supplied. This allows entry from notifications and the subscriptions list, since the data hasn't necessarily been fetched before viewing the details screens in those cases.

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 import {
     Text, View, ScrollView,
-    TouchableOpacity, StyleSheet
+    TouchableOpacity, StyleSheet, ActivityIndicator
 } from 'react-native';
 
 import DetailDisplay from '../components/DetailDisplay';
+
+// FIXME: this is a hack; we should be relying on the store rather than querying directly, but alas
+import {fetchDetails} from '../redux/actions';
 
 export default class DetailScreen extends Component {
     static navigatorStyle = {
@@ -13,12 +16,41 @@ export default class DetailScreen extends Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            data: this.props.data || null
+        };
     }
 
+    componentDidMount() {
+        console.log("Navigated to details screen");
+
+        // here we should check if data is null and if we have a variant_id
+        // if that's the case, then we perform a lookup and feed in the result
+        if (this.state.data === null && this.props.variant_id !== null) {
+            console.log("Fetching data because we weren't fed it; variant_id: ", this.props.variant_id);
+
+            fetchDetails(this.props.variant_id).then((d) => {
+                // console.log("Fetched data: ", d);
+
+                this.setState({
+                    data: d.data[0]
+                });
+            });
+        }
+    }
+
+    // FIXME: this component should deal with receiving a request for data via its variant_id prop
+
     render() {
+        let target = <ActivityIndicator style={{margin: 10}} size='large' animating={true} />;
+        if (this.state.data !== null) {
+            target = (<DetailDisplay data={this.state.data} />);
+        }
+
         return (
             <View style={styles.container}>
-                <DetailDisplay data={this.props.data} />
+            { target }
             </View>
         );
     }
@@ -27,7 +59,7 @@ export default class DetailScreen extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 20,
+        padding: 0,
         backgroundColor: 'white'
     },
     button: {
