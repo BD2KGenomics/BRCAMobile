d038673748660a69bc3627de93892899cc1b3296
Faisal <falquaddoomi@ucla.edu>
2017-07-18 11:57:52 +0200

    Added a method to check the status code of the response and throw an error if it's not OK, exported both methods in the file properly

--- src/toolbox/misc.js
+++ src/toolbox/misc.js
@@ -3,6 +3,16 @@
  */
 
 // utility method to combine [[1,2,3],[4,5,6]] into [[1,4],[2,5],[3,6]]
-function zip(rows) {
+export function zip(rows) {
     return rows[0].map((_,c)=>rows.map(row=>row[c]));
+}
+
+export function checkStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+        return response
+    } else {
+        const error = new Error(response.statusText);
+        error.response = response;
+        throw error
+    }
 }
