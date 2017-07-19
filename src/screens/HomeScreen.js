91e372190d5fbcb0768d4730d9f7ced094fca821
Faisal <falquaddoomi@ucla.edu>
2017-07-19 08:29:38 +0200

    Normalized indentation, fixed issue with hamburger menu mentioned in previous commit

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {
-  Text, TextInput, View, ScrollView, Image, TouchableOpacity, StyleSheet,
-  Alert, Platform
+    Text, TextInput, View, ScrollView, Image, TouchableOpacity, StyleSheet,
+    Alert, Platform
 } from 'react-native';
 import {Navigation} from 'react-native-navigation';
 import debounce from 'lodash/debounce';
@@ -10,101 +10,101 @@ import LinkableMenuScreen from './LinkableMenuScreen';
 import SearchBar from '../components/SearchBar';
 
 export default class HomeScreen extends LinkableMenuScreen {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchText: ''
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchText: ''
+        }
     }
-  }
 
-  static navigatorButtons = {
-    leftButtons: [{
-      icon: require('../../img/navicon_menu.png'),
-      id: 'sideMenu'
-    }]
-  };
+    static navigatorButtons = {
+        leftButtons: [{
+            icon: (Platform.OS === 'ios') ? require('../../img/navicon_menu.png') : null,
+            id: 'sideMenu'
+        }]
+    };
 
-  onChangeText(text) {
-    this.setState({
-      searchText: text
-    })
-  }
+    onChangeText(text) {
+        this.setState({
+            searchText: text
+        })
+    }
 
-  onSubmit() {
-    // leap to the search page if we have a query
-    let query = this.state.searchText.slice(0);
+    onSubmit() {
+        // leap to the search page if we have a query
+        let query = this.state.searchText.slice(0);
 
-    // FIXME: navigating to a different page clutters the search history; maybe having it on the same page is better
-    this.props.navigator.resetTo({
-      title: "Search",
-      screen: "brca.SearchScreen",
-      animated: false,
-      passProps: {
-        initialFilterText: query
-      }
-    });
+        // FIXME: navigating to a different page clutters the search history; maybe having it on the same page is better
+        this.props.navigator.resetTo({
+            title: "Search",
+            screen: "brca.SearchScreen",
+            animated: false,
+            passProps: {
+                initialFilterText: query
+            }
+        });
 
-    // clear the search box before we go
-    this.setState({ searchText: '' });
-  }
+        // clear the search box before we go
+        this.setState({ searchText: '' });
+    }
 
-  render() {
-    return (
-      <ScrollView style={{flex: 1, padding: 20, backgroundColor: 'white'}}>
-        <SearchBar
-          text={this.state.searchText}
-          autoFocus={false}
-          onChangeText={this.onChangeText.bind(this)}
-          onSubmit={this.onSubmit.bind(this)} />
+    render() {
+        return (
+            <ScrollView style={{flex: 1, padding: 20, backgroundColor: 'white'}}>
+                <SearchBar
+                    text={this.state.searchText}
+                    autoFocus={false}
+                    onChangeText={this.onChangeText.bind(this)}
+                    onSubmit={this.onSubmit.bind(this)} />
 
-        <View style={styles.info}>
-          <Text style={styles.paragraph}>The BRCA Exchange aims to advance our understanding of the genetic basis of breast cancer, ovarian cancer and other diseases by pooling data on BRCA1/2 genetic variants and corresponding clinical data from around the world. Search for BRCA1 or BRCA2 variants above.</Text>
+                <View style={styles.info}>
+                    <Text style={styles.paragraph}>The BRCA Exchange aims to advance our understanding of the genetic basis of breast cancer, ovarian cancer and other diseases by pooling data on BRCA1/2 genetic variants and corresponding clinical data from around the world. Search for BRCA1 or BRCA2 variants above.</Text>
 
-          <Text style={{fontSize: 5}}>&nbsp;</Text>
+                    <Text style={{fontSize: 5}}>&nbsp;</Text>
 
-          <Text style={styles.paragraph}>This project is supported by the BRCA Exchange of the Global Alliance for Genomics and Health.</Text>
-        </View>
+                    <Text style={styles.paragraph}>This project is supported by the BRCA Exchange of the Global Alliance for Genomics and Health.</Text>
+                </View>
 
-        <View style={styles.logo}>
-          <Image style={{width: 133, height: 67}} source={require('../../img/logos/brcaexchange.jpg')} />
-        </View>
-      </ScrollView>
-    );
-  }
+                <View style={styles.logo}>
+                    <Image style={{width: 133, height: 67}} source={require('../../img/logos/brcaexchange.jpg')} />
+                </View>
+            </ScrollView>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  button: {
-    textAlign: 'center',
-    fontSize: 18,
-    marginBottom: 10,
-    marginTop: 10,
-    color: 'blue'
-  },
-  info: {
-    backgroundColor: '#f1f1f1',
-    padding: 28,
-    borderRadius: 8
-  },
-  paragraph: {
-    color: '#555',
-    fontSize: 20
-  },
-  searchboxContainer: {
-    marginTop: 5,
-    marginBottom: 20
-  },
-  searchboxInput: {
-    borderWidth: 1,
-    padding: 6,
-    color: '#555',
-    borderColor: 'pink',
-    borderRadius: 4,
-    fontSize: 18
-  },
-  logo: {
-    marginTop: 30,
-    marginBottom: 120,
-    alignItems: 'center',
-  }
+    button: {
+        textAlign: 'center',
+        fontSize: 18,
+        marginBottom: 10,
+        marginTop: 10,
+        color: 'blue'
+    },
+    info: {
+        backgroundColor: '#f1f1f1',
+        padding: 28,
+        borderRadius: 8
+    },
+    paragraph: {
+        color: '#555',
+        fontSize: 20
+    },
+    searchboxContainer: {
+        marginTop: 5,
+        marginBottom: 20
+    },
+    searchboxInput: {
+        borderWidth: 1,
+        padding: 6,
+        color: '#555',
+        borderColor: 'pink',
+        borderRadius: 4,
+        fontSize: 18
+    },
+    logo: {
+        marginTop: 30,
+        marginBottom: 120,
+        alignItems: 'center',
+    }
 });
