559ddf2022dce21bb09a2e88ca122e00cfd6baab
Faisal <falquaddoomi@ucla.edu>
2017-09-06 09:16:07 +0200

    Removed some crufty includes in the screen mgmt. code that weren't being used.

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,15 +1,6 @@
 import { Navigation } from 'react-native-navigation';
 import { Provider } from 'react-redux';
 
-import HomeScreen from './HomeScreen';
-import SearchScreen from './SearchScreen';
-import AboutScreen from './AboutScreen';
-import DetailScreen from './DetailScreen';
-import SubscriptionsScreen from './SubscriptionsScreen';
-import SettingsScreen from "./SettingsScreen";
-import NotifyLogScreen from './NotifyLogScreen';
-import SideMenu from './SideMenu';
-
 import {screens} from '../metadata/screens';
 
 // register all screens of the app (including internal ones)
@@ -17,12 +8,4 @@ export function registerScreens(store) {
     screens.forEach((screen) => {
         Navigation.registerComponent(screen.name, () => screen.component, store, Provider);
     });
-
-    // Navigation.registerComponent('brca.HomeScreen', () => HomeScreen, store, Provider);
-    // Navigation.registerComponent('brca.SearchScreen', () => SearchScreen, store, Provider);
-    // Navigation.registerComponent('brca.AboutScreen', () => AboutScreen, store, Provider);
-    // Navigation.registerComponent('brca.SubscriptionsScreen', () => SubscriptionsScreen, store, Provider);
-    // Navigation.registerComponent('brca.DetailScreen', () => DetailScreen, store, Provider);
-    // Navigation.registerComponent('brca.SettingsScreen', () => SettingsScreen, store, Provider);
-    // Navigation.registerComponent('brca.SideMenu', () => SideMenu, store, Provider);
 }
