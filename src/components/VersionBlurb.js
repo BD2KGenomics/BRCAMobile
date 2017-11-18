2d77bf613e1525b5bca6241303e8003877252d81
Faisal <falquaddoomi@ucla.edu>
2017-11-18 12:23:38 +0100

    1.3.6

--- src/components/VersionBlurb.js
+++ src/components/VersionBlurb.js
@@ -46,7 +46,7 @@ class VersionBlurb extends Component {
     render() {
         return (
             <TouchableOpacity onPress={this.onTappedVersion}>
-                <Text style={styles.version}>brca-exchange mobile v1.3.5</Text>
+                <Text style={styles.version}>brca-exchange mobile v1.3.6</Text>
             </TouchableOpacity>
         );
     }
