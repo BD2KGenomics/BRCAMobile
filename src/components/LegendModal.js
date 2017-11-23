496cd604de229e1816b2cf8cf2cd8fc0679a6e41
Faisal <falquaddoomi@ucla.edu>
2017-11-23 11:29:15 +0100

    Legend modal now resizes itself to the screen's width and is scrollable. No longer dismissed by pressing anywhere, since the user needs to be able to interact with the modal to scroll it.

--- src/components/LegendModal.js
+++ src/components/LegendModal.js
@@ -28,53 +28,60 @@ export default class LegendModal extends Component {
             onRequestClose={() => this.props.onDismissLegend()}
             animationType="fade">
 
+            {/*
             <TouchableOpacity activeOpacity={1} focusedOpacity={1} style={{backgroundColor: 'rgba(0,0,0,0.25)', flexGrow: 1}} onPress={() => this.props.onDismissLegend()}>
+            */}
+
+            <View style={{backgroundColor: 'rgba(0,0,0,0.25)', position: 'absolute', top: 0, bottom: 0, left: 0, right: 0}}>
                 <View style={styles.legendModal}>
                     <View style={styles.legendModalHeader}>
                         {/*<Icon name="help" color="white" size={22} />*/}
                         <Text style={styles.legendModalTitle}>Icon Legend</Text>
                     </View>
 
-                    <View style={{padding: 20}}>
-                        <ScrollView>
-                            <Text style={styles.subheader}>Pathogenicity:</Text>
+                    <ScrollView style={{flex: 1, padding: 20, marginBottom: 20}}>
+                        <Text style={styles.subheader}>Pathogenicity:</Text>
+                        <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
+                        {
+                            Object.keys(patho_indicators).map((name, idx) => {
+                                const obj_style = patho_indicators[name];
 
-                            {
-                                Object.keys(patho_indicators).map((name, idx) => {
-                                    const obj_style = patho_indicators[name];
+                                return (
+                                    <View key={idx} style={{minWidth: 250, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', marginBottom: 15}}>
+                                        <Icon {...obj_style} size={22} />
+                                        <Text style={{marginLeft: 5, marginRight: 10}}>{ obj_style.title || name }</Text>
+                                    </View>
+                                );
+                            })
+                        }
+                        </View>
 
-                                    return (
-                                        <View key={idx} style={{flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', marginBottom: 15}}>
-                                            <Icon {...obj_style} size={22} />
-                                            <Text style={{marginLeft: 5, marginRight: 10}}>{ obj_style.title || name }</Text>
-                                        </View>
-                                    );
-                                })
-                            }
+                        <Text style={styles.subheader}>Follow Status:</Text>
+                        <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
+                        {
+                            Object.keys(follow_indicators).map((name, idx) => {
+                                const obj_style = follow_indicators[name];
 
-                            <Text style={styles.subheader}>Follow Status:</Text>
+                                return (
+                                    <View key={idx} style={{minWidth: 250, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', marginBottom: 15}}>
+                                        <Icon {...obj_style} size={22} />
+                                        <Text style={{marginLeft: 5, marginRight: 10}}>{name}</Text>
+                                    </View>
+                                );
+                            })
+                        }
+                        </View>
 
-                            {
-                                Object.keys(follow_indicators).map((name, idx) => {
-                                    const obj_style = follow_indicators[name];
+                        <Text>{' '}</Text>
+                    </ScrollView>
 
-                                    return (
-                                        <View key={idx} style={{flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', marginBottom: 15}}>
-                                            <Icon {...obj_style} size={22} />
-                                            <Text style={{marginLeft: 5, marginRight: 10}}>{name}</Text>
-                                        </View>
-                                    );
-                                })
-                            }
-                        </ScrollView>
-
+                    <View style={{padding: 20, paddingTop: 10, borderTopColor: "#eee", borderTopWidth: 1}}>
                         <TouchableOpacity style={styles.closeModalButton} onPress={() => this.props.onDismissLegend()}>
                             <Text style={styles.closeModalButtonText}>close</Text>
                         </TouchableOpacity>
                     </View>
                 </View>
-            </TouchableOpacity>
-
+            </View>
         </Modal>)
     }
 }
@@ -82,7 +89,9 @@ export default class LegendModal extends Component {
 const styles = StyleSheet.create({
     legendModal: {
         backgroundColor: 'white',
+        position: 'absolute',
         top: 0,
+        bottom: 0,
         borderColor: '#a6a6a6',
         borderWidth: 1,
         borderRadius: 3,
